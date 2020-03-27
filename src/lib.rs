4edd10889cc8dee3a35dcf69d3cac1aef2684f3f
Ben <benlaugherty@gmail.com>
2020-03-26 23:22:06 -0600

    add canvas version

--- src/lib.rs
+++ src/lib.rs
@@ -120,5 +120,17 @@ impl Universe {
 
         count
     }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
 }
 
